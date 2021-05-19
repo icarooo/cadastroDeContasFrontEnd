10dc86b84f3177e3aad7b133ccf5dfcede68afba
Icaro Pinheiro <icaro@ipr.net.br>
2021-05-19 01:13:46 -0300

    conta model

---
+++ src/app/conta.ts
@@ -0,0 +1,12 @@
+export class Conta {
+    id!: number;
+    codBanco!: string;
+    nomeBanco!: string;
+    numeroConta!: string;
+    agencia!: string;
+    cpfCnpj!: string;
+    nome!: string;
+    dataAbertura!: Date;
+    ativo!: boolean;
+
+}
