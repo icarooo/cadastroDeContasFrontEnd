ed9ed66dc4eac5ef3c741357d3473412944a1739
Icaro Pinheiro <icaro@ipr.net.br>
2021-05-21 19:48:38 -0300

    atualizando services

--- src/app/conta.service.ts
+++ src/app/conta.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';  
 import { HttpHeaders } from '@angular/common/http';  
 import { Observable } from 'rxjs';  
-import { Conta } from './conta'; 
-import { Bancos } from './banco';
+import { Conta } from './models/conta'; 
+import { Bancos } from './models/banco';
 
 var httpOptions = {headers: new HttpHeaders({"Content-Type": "application/json"})};
 
